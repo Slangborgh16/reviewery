56f81e6e6edf3c8efddd9f3c1ab3f3fd85514cab
sfl <slangborgh16@gmail.com>
2023-04-16 00:25:30 -0400

    Testing text replacement in Markdown files for templates

--- markdown_to_pdf_test.py
+++ markdown_to_pdf_test.py
@@ -3,13 +3,14 @@ import weasyprint
 from pathlib import Path
 
 test_file = Path('test.md')
+output_pdf = Path('test.pdf')
 
 with open(test_file, 'r', encoding='utf-8') as f:
     text = f.read()
+
+text = text.replace('{title}', 'TEST TITLE').replace('{year}', str(2023)).replace('{artist}', 'ARTIST NAME')
+
 html = markdown.markdown(text)
 
-weasyprint.HTML(string=html).write_pdf('test.pdf')
-
-# with open('test.html', 'w', encoding='utf-8') as f:
-#     f.write(html)
-
+css = weasyprint.CSS(string='body { font-family: calibri }')
+weasyprint.HTML(string=html).write_pdf(output_pdf, stylesheets=[css])
