d34b4bcd1abe1db3c6ba4db3a431a4b2a85fba27
sfl <slangborgh16@gmail.com>
2023-04-16 23:23:38 -0400

    Started implementing generate PDF functionality

--- reviewery.py
+++ reviewery.py
@@ -1,7 +1,9 @@
-import sys
 import os
+import sys
 import argparse
+import weasyprint
 import pandas as pd
+import markdown as md
 from pathlib import Path
 
 
@@ -13,19 +15,24 @@ def get_cmd_args():
     add_parser = commands.add_parser('add', help='add a new entry to an existing review database')
     generate_parser = commands.add_parser('generate', help='generate a PDF from the database and template files')
 
-    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose output')
+    # parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose output')
 
-    create_parser.add_argument('output_file', type=Path, help='output database CSV file path')
+    create_parser.add_argument('-o', '--output', type=Path, help='output database CSV file path', required=True)
 
     add_parser.add_argument('database_file', type=Path, help='path to existing review database file')
 
-    generate_parser.add_argument('output_file', nargs='?', default='./reviews.pdf', type=Path, help='output PDF file path (default: %(default)s)')
+    generate_parser.add_argument('database_file', type=Path, help='path to existing reviews database file')
+    generate_parser.add_argument('-d', '--dir', default='.', type=Path, help='directory that contains review write-up files')
+    generate_parser.add_argument('-s', '--sort', action='append',
+            help='attribute to organize pdf by. can be used more than once to sort by multiple attributes')
+    generate_parser.add_argument('-o', '--output', default='./reviews.pdf', type=Path, help='output PDF file path (default: %(default)s)')
+    generate_parser.add_argument('-t', '--template', type=Path, help='template file used to generate pages', required=True)
 
     return parser.parse_args()
 
 
 def create(args: argparse.Namespace):
-    out_file: Path = args.output_file.resolve()
+    out_file: Path = args.output.resolve()
     print(f'Creating reviews database "{out_file.name}" in "{out_file.parent}"\n')
     # print('Attributes are information about each review such as an album title, and author, a director, etc.')
     # print('Attributes can also be an image URL or path. All spaces in the attribute name are automatically replaced with underscores.')
@@ -39,7 +46,7 @@ def create(args: argparse.Namespace):
     attributes: list[str] = []
 
     while True:
-        attr = input(f'{num_attribute}. ')
+        attr: str = input(f'{num_attribute}. ')
         if attr.isspace() or attr == '':
             if len(attributes) == 0:
                 print('No attributes entered. Database will not be created.')
@@ -56,7 +63,7 @@ def create(args: argparse.Namespace):
                 for a in attributes:
                     print(a, end=' ')
             
-                choice = input('\n(F)inish / (q)uit / (c)ontinue / (r)estart: ')
+                choice: str = input('\n(F)inish / (q)uit / (c)ontinue / (r)estart: ')
 
                 if choice in ('f', 'F', 'finish'):
                     break
@@ -85,6 +92,7 @@ def create(args: argparse.Namespace):
         print('Done')
     except OSError as error:
         print(f'Failed writing CSV to {out_file}:\n\t{error}')
+        return
     
 
 def add_entry(args: argparse.Namespace):
@@ -97,30 +105,59 @@ def add_entry(args: argparse.Namespace):
         return
 
     try:
-        df = pd.read_csv(db_file)
+        df: pd.DataFrame = pd.read_csv(db_file)
     except OSError as error:
         print(f'Error opening {db_file}:\n\t{error}')
+        return
 
     while True:
         print('Enter values for each attribute.')
-        new_entry = []
+        new_entry: list[str] = []
         
         for attr in list(df.keys()):
-            val = input(f'{attr}: ')
+            val: str = input(f'{attr}: ')
             new_entry.append(val)
 
         df.loc[len(df.index)] = new_entry
         df.to_csv(db_file, index=False)
 
-        choice = input('Added entry. Would you like to add another? (Y/n): ')
+        choice: str = input('Added entry. Would you like to add another? (Y/n): ')
 
         if choice in ('n', 'N', 'no'):
             break
 
 
 def generate(args: argparse.Namespace):
-    print('Generate!')
-    print(args.verbose)
+    writeup_dir: Path = args.dir.resolve()
+    out_file: Path = args.output.resolve()
+    template_file: Path = args.template.resolve()
+    db_file: Path = args.database_file.resolve()
+
+    sort_by: list[str] = args.sort
+
+    try:
+        with open(template_file) as f:
+            template: str = f.read()
+    except OSError as error:
+        print(f'Error opening {template_file}:\n\t{error}')
+        return
+    
+    try:
+        df: pd.DataFrame = pd.read_csv(db_file)
+    except OSError as error:
+        print(f'Error opening {db_file}:\n\t{error}')
+        return
+
+    doc: str = ''
+
+    for _, review in df.iterrows():
+        review_md: str = template
+        for attr in list(df.keys()):
+            review_md = review_md.replace(f'{{{attr}}}', review[attr])
+        doc += md.markdown(review_md)
+
+    css = weasyprint.CSS(string='body { font-family: calibri }')
+    weasyprint.HTML(string=doc).write_pdf(out_file, stylesheets=[css])
 
 
 if __name__ == '__main__':
